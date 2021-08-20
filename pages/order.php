776c2de8b862c85dcd217e25d24ce9ea5674e50d
coderme777 <85622586+coderme777@users.noreply.github.com>
2021-08-20 11:16:39 +0300

    Update order.php

--- pages/order.php
+++ pages/order.php
@@ -1,8 +1,9 @@
-<?require_once('fs_db.php');?>
+<?php require_once('fs_db.php');?>
 <div id='carzin'>
 	<h2>Оформление заказа</h2>
 	<?php
-	if($_SESSION['cart'] && !isset($_POST['order'])){//если существует что-то в корзине и кнопка формы не нажата, то выводим данные ниже
+	if($_SESSION['cart'] && !isset($_POST['order']))
+	{//если существует что-то в корзине и кнопка формы не нажата, то выводим данные ниже
 	?>	
 	<form action='index.php?view=order' method='post' >
 			<?php
@@ -14,7 +15,7 @@
 		<p>Цена: <?=number_format($prod[0]['price'],2)?>$</p>
 		<p>Кол-во: <?=$quant?></p>
 		<p>Всего: <?=number_format($prod[0]['price']*$quant,2)?>$</p>
-	<?endforeach;?>
+	<?php endforeach;?>
 		<p class='total'>Общая сумма заказа: <span class='product-price'><?=number_format($_SESSION['total_price'],2)?>$</span></p>
 		<p>Имя:<br><input type='text' name='name'></p>
 		<p>Фамилия:<br><input type='text' name='s_name'></p>
@@ -24,15 +25,18 @@
 		<p><input type='submit' name='order' value='Заказать'></p>		
 	</form>
 </div>
-<?php }else{
-	if($_SESSION['cart'] && isset($_POST['order'])){
-		$date = date('Y-m-d');
-		$time = date('H:i:s');
-		$name = htmlspecialchars($_POST['name']);
-		$s_name = htmlspecialchars($_POST['s_name']);
-		$email = htmlspecialchars($_POST['email']);
-		$address = htmlspecialchars($_POST['address']);
-		$post_index = htmlspecialchars($_POST['post_index']);
+<?php }
+	else
+	{
+		if($_SESSION['cart'] && isset($_POST['order']))
+		{
+			$date = date('Y-m-d');
+			$time = date('H:i:s');
+			$name = htmlspecialchars($_POST['name']);
+			$s_name = htmlspecialchars($_POST['s_name']);
+			$email = htmlspecialchars($_POST['email']);
+			$address = htmlspecialchars($_POST['address']);
+			$post_index = htmlspecialchars($_POST['post_index']);
 		
 		foreach($_SESSION['cart'] as $id => $quant):
 				$prod=getproduct($id);
